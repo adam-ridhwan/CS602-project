ae9a39a2af888bada9e64911fb0a39368eb3b855
Adam Ridhwan <adamridhwan.1@gmail.com>
2022-11-15 13:19:48 -0500

    created dropdown for sort

--- src/Components/Home/Adjustments/SortButton/SortButton.jsx
+++ src/Components/Home/Adjustments/SortButton/SortButton.jsx
@@ -1,11 +1,72 @@
+import { useEffect, useRef, useState } from 'react';
 import './SortButton.styles.css';
 
 const SortButton = () => {
+  const [isSortDrpdwnOpen, setIsSortDrpdwnOpen] = useState(false);
+  const sortBtnRef = useRef();
+
+  useEffect(() => {
+    const sortBtnHandler = event => {
+      if (!sortBtnRef.current.contains(event.target)) {
+        setIsSortDrpdwnOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', sortBtnHandler);
+    return () => {
+      document.removeEventListener('mousedown', sortBtnHandler);
+    };
+  });
+
+  useEffect(() => {
+    const sortDrpdwn = sortBtnRef.current;
+
+    if (isSortDrpdwnOpen) {
+      sortDrpdwn.classList.add('active');
+      return;
+    } else {
+      sortDrpdwn.classList.remove('active');
+    }
+  }, [isSortDrpdwnOpen]);
+
+  useEffect(() => {
+    console.log(isSortDrpdwnOpen);
+  }, [isSortDrpdwnOpen]);
+
   return (
     <>
-      <button className='sorting'>
-        <p>Sort</p>
-      </button>
+      <div ref={sortBtnRef} className='sort-container'>
+        <button
+          className='link'
+          onClick={() =>
+            setIsSortDrpdwnOpen(isSortDrpdwnOpen => !isSortDrpdwnOpen)
+          }
+        >
+          <p>Sort</p>
+        </button>
+
+        <div className='dropdown-menu'>
+          <div>
+            <p>Ascending</p>
+          </div>
+
+          <div>
+            <p>Descending</p>
+          </div>
+
+          <hr
+            style={{
+              height: '2px',
+              backgroundColor: 'rgb(244,244,244)',
+              border: 'none',
+              margin: '7.5px 0',
+            }}
+          />
+
+          <div>
+            <p>Remove Sort</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
